c375f6d0ac101aa7855144f6173f0aec947c8cf8
Hesam N <AnonCODER@tutanota.com>
2022-01-19 15:51:25 +0330

    Start project
    Django project added

---
+++ C1Academy/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for C1Academy project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'C1Academy.settings')
+
+application = get_wsgi_application()
